46abbafb5b3c090c0ee6a86f62214666e9a6aecf
MisnikovRoman <36447757+MisnikovRoman@users.noreply.github.com>
2019-01-10 23:22:08 +0300

    (fix) убрал лишние пробелы и заглавную букву
    Лучше использовать только маленькие первые буквы в названиях переменных

--- app/src/main/java/com/example/user/weatherapp/model_view/ModelView.java
+++ app/src/main/java/com/example/user/weatherapp/model_view/ModelView.java
@@ -3,11 +3,8 @@ package com.example.user.weatherapp.model_view;
 public class ModelView {
 
     private String city;
-    private int Id;
+    private int id;
     private float temp;
     private float homidity;
-
-
-
-
+    
 }
